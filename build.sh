42f8f563ce467d3ab61a5bc45938e25bd5012351
Neill Bogie <neillbogie@googlemail.com>
2013-05-10 00:28:06 +0100

    in build.sh remove profiling artifacts first

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -e
 set -u
+rm -f *.hp *.aux *.ps
+
 clear; ghc --make -Wall -O2 -prof -caf-all -auto-all -fforce-recomp brainfk_interpreter.hs 
 time ./brainfk_interpreter r +RTS -s -h < samples/99botles.bf 
 hp2ps brainfk_interpreter.hp
