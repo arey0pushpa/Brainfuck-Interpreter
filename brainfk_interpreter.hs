82e2a6d32e097ddda92e5480328105b07ce91e8b
Neill Bogie <neillbogie@googlemail.com>
2013-05-10 15:06:05 +0100

    tidy

--- brainfk_interpreter.hs
+++ brainfk_interpreter.hs
@@ -1,3 +1,4 @@
+-- Incomplete, as it doesn't do input
 module Main where
 import Data.Array
 import Data.Char (chr)
@@ -9,8 +10,8 @@ type Byte = Int
 
 data World = World { wpc     :: Int
                    , pointer :: Int
-                   , program :: (Array Int Instruction)
-                   , memory  :: (M.Map Int Byte)
+                   , program :: Array Int Instruction
+                   , memory  :: M.Map Int Byte
                    , output  :: [Byte]
                    } deriving (Show)
 
@@ -40,21 +41,30 @@ main = do
   _counts <- tests
   let f = case mode of
             ShowProg -> parseAndShow
-            RunProg  -> parseAndRun
+            RunProg  -> parseRunAndDisplay
   interact f
 
-parseToArray ::  String -> Array Int Instruction
-parseToArray = toInstructionArray . parse
+parseRunAndDisplay ::  String -> String
+parseRunAndDisplay = display . run . parse
+
+parseAndShow ::  String -> String
+parseAndShow = unlines . map show . zip [(0::Int)..] . parse
+
+data Instruction = IncP
+                 | DecP
+                 | IncB
+                 | DecB
+                 | OutB
+                 -- | InpB
+                 | JmpF
+                 | JmpB
+                 deriving (Show, Enum, Eq, Ord)
 
 toInstructionArray :: [Instruction] -> Array Int Instruction
 toInstructionArray cmds = array (0, length cmds - 1) $ zip [0..] cmds
 
-parseAndRun ::  String -> String
-parseAndRun = run . parse
-parseAndShow ::  String -> String
-parseAndShow = unlines . map show . zip [(0::Int)..] . parse
-
-data Instruction = IncP | DecP | IncB | DecB | OutB | InpB | JmpF | JmpB deriving (Show, Enum, Eq, Ord)
+parseToArray ::  String -> Array Int Instruction
+parseToArray = toInstructionArray . parse
 
 parse :: String -> [Instruction]
 parse cs = map parseOne $ filter legalInstructionChar cs
@@ -65,7 +75,7 @@ parseOne '<' = DecP
 parseOne '+' = IncB
 parseOne '-' = DecB
 parseOne '.' = OutB
-parseOne ',' = InpB
+-- parseOne ',' = InpB -- TODO: implement InputByte instruction
 parseOne '[' = JmpF
 parseOne ']' = JmpB
 parseOne other = error $ "illegal character: " ++ [other]
@@ -73,14 +83,17 @@ parseOne other = error $ "illegal character: " ++ [other]
 legalInstructionChar :: Char -> Bool
 legalInstructionChar = flip elem "><+-.,[]"
 
-run :: [Instruction] -> String
-run cmds = let w = initWorld cmds
+display :: [Byte] -> String
+display = map chr . reverse
+
+run :: [Instruction] -> [Byte]
+run cmds = let w  = initWorld cmds
                w' = run' w
-           in map chr $ reverse $ output $ w' 
+           in output w' 
 
 run' :: World -> World
 run' w = case nextInstruction w of
-  Nothing -> w
+  Nothing    -> w
   Just instr -> run' next
     where next = applyAndAdvance instr w
 
@@ -91,7 +104,7 @@ nextInstruction w
   where 
     lastInstrAddr = snd $ bounds $ program w
 
-applyAndAdvance ::  Instruction -> World -> World
+applyAndAdvance :: Instruction -> World -> World
 applyAndAdvance instr w = pcChange $ apply instr w
   where
     pcChange = if handlesPC instr then id else incPC
@@ -100,12 +113,6 @@ applyAndAdvance instr w = pcChange $ apply instr w
     handlesPC JmpB = True
     handlesPC _ = False
 
-incPC ::  World -> World
-incPC w = w { wpc = wpc w + 1 }
-modPointer :: (Int -> Int) -> World -> World
-modPointer op w = w { pointer = newVal }
-  where newVal = op (pointer w)
-
 apply ::  Instruction -> World -> World
 apply IncP w = modPointer (+1) w
 apply DecP w = modPointer (subtract 1) w
@@ -113,43 +120,51 @@ apply IncB w = modByteAtPointer (+1) w
 apply DecB w = modByteAtPointer (+(-1)) w
 apply OutB w = w { output = newVal }
   where newVal = byteAtPointer w : output w 
-apply InpB _w = error "Not implemented Input Byte"
+-- TODO: implement InputByte instruction
+-- apply InpB _w = error "Not implemented Input Byte"
 apply JmpF w = if byteAtPointer w == 0 then jumpForward w else incPC w
 apply JmpB w = if byteAtPointer w /= 0 then jumpBackward w else incPC w
+
+data JumpDir = Back | Forward deriving (Eq)
 
 jumpBackward ::  World -> World
 jumpBackward = modPC jb
 jumpForward ::  World -> World
 jumpForward = modPC jf
 
-modPC ::  (Int -> Array Int Instruction -> Int) -> World -> World
-modPC f w = w { wpc = pc' }
-  where pc' = f (wpc w) (program w)
-
 jf :: Int -> Array Int Instruction -> Int
 jf pc arr = jump pc arr Forward
+
 jb :: Int -> Array Int Instruction -> Int
 jb pc arr = jump pc arr Back
-
-data JumpDir = Back | Forward deriving (Eq)
 
 jump :: Int -> Array Int Instruction -> JumpDir -> Int
 jump progc arr dir = jf' progc 0
   where
-    (less, more) = lessAndMoreFor dir
-    inc = pcIncFor dir
     jf' :: Int -> Int -> Int
-    jf' pc depth | bt == less   = (if depth == 0 then pc'+1 else jf' pc' (depth-1))
-                 | bt == more   = jf' pc' (depth + 1)
+    jf' pc depth | byt == less  = if depth == 0 then pc'+1 else jf' pc' (depth-1)
+                 | byt == more  = jf' pc' (depth + 1)
                  | otherwise    = jf' pc' depth
       where
-        bt = arr ! pc' 
+        byt = arr ! pc' 
         pc' = pc + inc
+    (less, more)           = lessAndMoreFor dir
     lessAndMoreFor Forward = (JmpB, JmpF)
     lessAndMoreFor Back    = (JmpF, JmpB)
-    pcIncFor Forward = 1
-    pcIncFor Back    = -1
+    inc                    = pcIncFor dir
+    pcIncFor Forward       = 1
+    pcIncFor Back          = -1
 
+incPC ::  World -> World
+incPC w = w { wpc = wpc w + 1 }
+
+modPC ::  (Int -> Array Int Instruction -> Int) -> World -> World
+modPC f w = w { wpc = pc' }
+  where pc' = f (wpc w) (program w)
+
+modPointer :: (Int -> Int) -> World -> World
+modPointer op w = w { pointer = newVal }
+  where newVal = op (pointer w)
 
 byteAtPointer ::  World -> Byte
 byteAtPointer w = memory w M.! pointer w
@@ -158,7 +173,6 @@ modByteAtPointer :: (Byte -> Byte) -> World ->  World
 modByteAtPointer op w = w { memory = newMem }
   where
     newMem = M.adjust (cap . op) (pointer w) (memory w)
-
 
 cap :: Byte -> Byte
 cap v | v < -128  = v + 256
@@ -169,7 +183,12 @@ cap v | v < -128  = v + 256
 -- TODO: add case when there is no program to run or at least when the program is one instruction long.
 --
 tests ::  IO Counts
-tests = runTestTT $ TestList [ jfTests, capTests]
+tests = runTestTT $ TestList [ jfTests, capTests, runnerTests]
+runnerTests :: Test
+runnerTests = TestList [ "no inp, no output" ~: []  ~=? (run $ parse "") 
+                       , "single instr"      ~: [0] ~=? (run $ parse ".")
+                       ]
+
 capTests :: Test
 capTests = TestList [ 127  ~=? cap (-129)
                     , 126  ~=? cap (-130)
